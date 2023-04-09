f3b7c230c336df78332524016bf9b6cb09f7f952
Mohidul Islam <mohidul.islam@volue.com>
2023-04-10 01:49:44 +0600

    Added CI pipeline for testing and added some test for controller

---
+++ controllers/controllers_test.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	return r
+}
+
+func TestHealthCheckRoute(t *testing.T) {
+	router := setupRouter()
+	healthController := new(HealthController)
+	router.GET("/health", healthController.Status)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/health", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Working!", w.Body.String())
+}
