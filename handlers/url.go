b0d3a7b8eae8c5d4e9c5ab72ecfcee06f93a0cb8
Mohidul Islam <mohidul.islam@volue.com>
2023-07-28 17:47:02 +0600

    more next level code leveraging go-routine and channel to achieve some parallelism

--- handlers/url.go
+++ handlers/url.go
@@ -2,14 +2,14 @@ package handlers
 
 import (
 	"context"
-	"errors"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mohidex/shorturl/db"
 	"github.com/mohidex/shorturl/models"
 	"github.com/mohidex/shorturl/utils"
-	"gorm.io/gorm"
+	"golang.org/x/sync/errgroup"
 )
 
 type ShortURLHandler struct {
@@ -26,8 +26,8 @@ func (h *ShortURLHandler) APIGenerateShortUrl(c *gin.Context) {
 		})
 		return
 	}
-	originalUrl := input.OriginalUrl
-	shortUrl, err := utils.GenerateShortLink(originalUrl)
+	longURL := input.OriginalUrl
+	shortUrl, err := utils.GenerateShortLink(longURL)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err,
@@ -37,25 +37,41 @@ func (h *ShortURLHandler) APIGenerateShortUrl(c *gin.Context) {
 
 	url := models.ShortURL{
 		ShortUrl: shortUrl,
-		DestUrl:  input.OriginalUrl,
+		DestUrl:  longURL,
 	}
 
-	err = h.PgDB.SetLongURL(context.Background(), url.ShortUrl, url.DestUrl)
+	// Create a bool channel to receive the result of the PostgreSQL save operation
+	pgSaveResult := make(chan bool, 1)
 
-	if err != nil && errors.Is(err, gorm.ErrDuplicatedKey) {
-		c.JSON(http.StatusConflict, gin.H{
-			"error": err.Error(),
-		})
-		return
-	} else if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
+	// Use Goroutines to save to both Redis and PostgreSQL simultaneously
+	ctx := context.Background()
+
+	go func() {
+		if err := h.PgDB.SetLongURL(ctx, url.ShortUrl, url.DestUrl); err != nil {
+			log.Printf("Failed to save short URL to PostgreSQL: %s\n", err)
+			pgSaveResult <- false // Signal failure to the bool channel
+			return
+		}
+		pgSaveResult <- true // Signal success to the bool channel
+	}()
+
+	// Wait for the PostgreSQL save operation result
+	if success := <-pgSaveResult; success {
+		// PostgreSQL save operation was successful, proceed to save to Redis
+		go func() {
+			if err := h.RedisDB.SetLongURL(ctx, url.ShortUrl, url.DestUrl); err != nil {
+				log.Printf("Failed to save long URL to Redis: %s\n", err)
+			}
+		}()
+	} else {
+		// PostgreSQL save operation failed, handle the error here if needed
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create short URL"})
 		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
-		"url": url,
+		"url":  url.DestUrl,
+		"code": url.ShortUrl,
 	})
 }
 
@@ -69,28 +85,34 @@ func (h *ShortURLHandler) GetShortUrl(c *gin.Context) {
 		return
 	}
 
-	if longUrl, err := h.RedisDB.GetLongURL(context.Background(), shortCode); err == nil {
-		c.JSON(http.StatusOK, gin.H{
-			"shortUrl": shortCode,
-			"fullUrl":  longUrl,
-		})
-		return
-	}
+	// Use an errgroup to handle multiple goroutines and their potential errors
+	g, ctx := errgroup.WithContext(c.Request.Context())
 
-	longUrl, err := h.PgDB.GetLongURL(context.Background(), shortCode)
+	// Create a variable to store the fetched long URL
+	var longURL string
 
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{
-			"error": "URL not found",
-		})
-		return
-	}
-
-	// since url found in the database which is not in cache. So setting it in cache
-	_ = h.RedisDB.SetLongURL(context.Background(), shortCode, longUrl)
-
-	c.JSON(http.StatusOK, gin.H{
-		"shortUrl": shortCode,
-		"fullUrl":  longUrl,
+	// Try fetching the long URL from Redis first
+	g.Go(func() error {
+		var err error
+		longURL, err = h.RedisDB.GetLongURL(ctx, shortCode)
+		return err
 	})
+
+	// Wait for the result from Redis cache response
+	if err := g.Wait(); err != nil {
+		// Redis query failed or URL not found in Redis, try fetching from postgreSQL database
+		var err error
+		longURL, err = h.PgDB.GetLongURL(context.Background(), shortCode)
+		if err != nil {
+			// If URL not found in PostgreSQL as well, return a 404
+			c.JSON(http.StatusNotFound, gin.H{"error": "Short URL not found"})
+			return
+		}
+		// since url found in the database which is not in cache. So setting it in cache
+		_ = h.RedisDB.SetLongURL(context.Background(), shortCode, longURL)
+
+	}
+
+	// Return the long URL to the client
+	c.Redirect(http.StatusFound, longURL)
 }
